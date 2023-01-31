adffacc1780333ae09921d620f2e8cbb3e3aab64
Erik M. Dodd <emdodd27@gmail.com>
2023-01-30 17:02:06 -0800

    Fixed random bug. Everything works

--- cars_api_project/urls.py
+++ cars_api_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("cars.urls")),
     path("api/v1/cars", include("cars.urls"))
 
 ]
